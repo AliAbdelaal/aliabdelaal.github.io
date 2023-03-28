f1fe592511bc00fbc8b4a8c03a1619fb95ce2408
AliAbdelaal <AliAbdelaal@users.noreply.github.com>
2023-03-28 00:45:11 +0000

    jekyll build from Action 7bf2be875f4fac01c3547f9ba57ea1c2632100c6

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Telegram bot tutorial using python and flask",
+        "excerpt":"img source in this tutorial you will learn how to make a telegram bot and deploy it on a heroku server, the bot will be up 24-7, and you can inject your own brain and responses into it. Before we start you will need to have the following a heroku...","categories": ["Blog"],
+        "tags": ["python","chatbot"],
+        "url": "/blog/telegram-bot-tutorial/",
+        "teaser": "https://www.wikitechy.com/technology/wp-content/uploads/2017/05/Telegram-Bots.png"
+      },{
+        "title": "مقدمة في علم الآلة",
+        "excerpt":"مجال تعلم الآلة او (machine learning) هو احد فروع الذكاء الاصطناعي و يشتمل بداخله علم التعلم العميق (deep learning) و عرفه العالم آرثر سامويل ان تعطي اجهزة الكمبيوتر القدرة على التعلم بدون ان تتم برمجتها بشكل صريح المصدر لتوضيح هذا العنوان بشكل افضل، لنستعرض الفرق بين البرمجة الحرفية و تعليم...","categories": ["Blog"],
+        "tags": ["machine learning","arabic"],
+        "url": "/blog/intro-to-ml/",
+        "teaser": "https://images.unsplash.com/photo-1527430253228-e93688616381?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1491&q=80"
+      },{
+        "title": "النزول التدريجي و بعض مشتقاته و استخدامهم في علم الآلة",
+        "excerpt":"مقدمة في هذا المقال سنتحدث عن خوارزمية النزول التدريجي او (Gradient Descent) وهي تعد اشهر الطرق و اكثرها استخداما في حل المعادلات المستخدمة في خوارزميات تعلم الآلة (machine learning) و كذلك التعلم العميق (deep learning). سوف نتعرض لكيفية عمل النزول التدريجي و اهميته و المشاكل التي نتعرض لها مع استخدامه...","categories": ["Blog"],
+        "tags": ["arabic","optimization","machine learning"],
+        "url": "/blog/gradient-descent-family/",
+        "teaser": "https://images.unsplash.com/photo-1454496522488-7a8e488e8606?ixlib=rb-1.2.1&auto=format&fit=crop&w=1355&q=80"
+      },{
+        "title": "Introduction to NLP in Arabic - part 1",
+        "excerpt":"مقدمة في هذا المقال المكون من جزئين سوف نلقي نظرة عامة على مجال معالجة النصوص باستخدام خوارزميات تعلم الآلة وسوف نلقى الضوء على تطور المجال بداية من استخدام الطرق التقليدية في معالجة النصوص مثل استخدام اسلوب (bag of words) التي تعتمد على عدد الكلمات إلى استخدام الطرق الحديثة مثل (word...","categories": ["Blog"],
+        "tags": ["arabic","natural language processing","machine learning"],
+        "url": "/blog/intro-to-nlp-p1/",
+        "teaser": "https://unsplash.com/photos/6jlYDFfyuCQ/download?force=true"
+      },{
+        "title": "Permutations and Combinations in Arabic",
+        "excerpt":"مقدمة اغلبنا يتذكر مصطلح التباديل و التوافيق من المدرسة، غالبا نتذكر الاسم فقط و ننسى ما الفارق بينهم او غالبا ننساهم بالكلية، في هذا المقال سوف نقوم بتبسيط المعلومة حتى تصبح سهلة الاسترجاع فلا نضطر لحفظ المعادلات بعد الان لنعلم على سبيل المثال ما عدد كلمات المرور الممكنة في القفل...","categories": ["Blog"],
+        "tags": ["arabic","math"],
+        "url": "/blog/permutations-and-combinations/",
+        "teaser": "https://unsplash.com/photos/Oal07Ai4oTk/download?force=true"
+      },{
+        "title": "Introduction to NLP in Arabic - part 2",
+        "excerpt":"مقدمة هذا المقال تكملة للمقال السابق، يمكنك الاطلاع عليه من هنا في المقال السابق تحدثنا عن نماذج مختلفة لمعالجة النصوص باستخدام خوارزميات تعلم الآلة، وتوقفنا عند بعض النقاط عند كل نموذج، دعنا نراجعها سريعا. ذكرنا في المرة السابقة استخدام Bag of words و وضحنا ان هذا النموذج لايأخذ في الحسبان...","categories": ["Blog"],
+        "tags": ["arabic","natural language processing","machine learning"],
+        "url": "/blog/intro-to-nlp-p2/",
+        "teaser": "https://unsplash.com/photos/LBKxu77KpSY/download?force=true"
+      }]
